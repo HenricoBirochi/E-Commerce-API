ec8577511297f20079af8618b036834290a16eac
HenricoBirochi <henrico.birochi@gmail.com>
2025-10-16 18:22:53 -0300

    feat: Add product retrieval and update functionality, enhance product listing API and User class added

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app) # Serve para usar outras ferramentas para testar a API, tipo o swagger
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -30,6 +38,49 @@ def delete_product(product_id):
         db.session.commit()
         return jsonify({"message": "Product deleted successfully"}), 200
     return jsonify({"message": "Product not found"}), 404
+
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        }), 200
+    return jsonify({"message": "Product not found"}), 404
+
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+    if 'price' in data:
+        product.price = data['price']
+    if 'description' in data:
+        product.description = data['description']
+
+    db.session.commit()
+    return jsonify({"message": "Product updated successfully"}), 200
+
+@app.route('/api/products', methods=['GET'])
+def list_products():
+    products = Product.query.all()
+    if not products:
+        return jsonify({"message": "No products found"}), 404
+    product_list = []
+    for product in products:
+        product_list.append({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price
+        })
+    return jsonify(product_list), 200
 
 @app.route('/')
 def hello_world():
